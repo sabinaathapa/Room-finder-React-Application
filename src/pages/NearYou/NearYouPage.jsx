26c68884617bf6ab567ff842d10b51571978bd56
biplaw <biplaw.chaudhary@codehimalaya.net>
2024-03-17 00:25:20 +0545

    Updated the route app with another component for location_access

---
+++ src/pages/NearYou/NearYouPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderCommon from "../../components/HeaderFooter/Header";
+import Footer from "../../components/headerfooter/Footer";
+import LocationAccess from "./LocationAccessPage";
+
+const NearYouPage = () =>{
+    return <>
+        <HeaderCommon/>
+        <LocationAccess/>
+        <Footer/>
+    </>
+}
+export default NearYouPage;
