291f4b3bfc4b56ebe572eee421f0c31815b6ad67
biplaw <biplaw.chaudhary@codehimalaya.net>
2024-03-19 18:25:02 +0545

    Updated the location denied page styling

--- src/pages/NearYou/LocationDeniedPage.jsx
+++ src/pages/NearYou/LocationDeniedPage.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HeaderCommon from "../../components/HeaderFooter/Header";
+import Footer from "../../components/headerfooter/Footer";
+import { Container, Row , Button} from "react-bootstrap";
 
 const LocationDeniedPage = () => {
   return (
-    <div className="text-center">
-      <h1>Oops!</h1>
-      <p>You did not allow access to your location.</p>
-      <Link to='/'>Back to Home Page</Link>
+    <>
+    <HeaderCommon/>
+    <div className="text-center my-5">
+      {/* <h1>Oops!</h1>
+      <p>You did not allow access to your location.</p> */}
+
+      <Container>
+        <Row className="justify-content-center">
+        <img src="src\assets\error1.gif" style={{ width: '70%' }}></img>
+        </Row>
+        <Row>
+        <Link to='/'><Button variant="outline-primary my-2" >Go Back to Homepage</Button></Link>
+      </Row>
+      </Container>
+      
+      
+   
     </div>
+    <Footer/>
+    </>
   );
 };
 
